9eb78d8787dde73781470da666dfbfc5ecd1bf02
ak01a <96882081+ak01a@users.noreply.github.com>
2024-08-26 09:12:36 +0530

    Python3 Solution - Iterative
    Submission Statistics:
    Question #: 776
    Language: Python3
    Runtime: 44 ms
    Runtime percentile: 66.50
    Memory: 18.092 MB
    Memory percentile: 95.62

---
+++ 776. N-ary Tree Postorder Traversal/Solution.py
@@ -0,0 +1,25 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        res =[]
+        if not root:
+            return[]
+        
+        stack = [(root,False)]
+
+        while stack:
+            node,visited = stack.pop()
+            if visited:
+                res.append(node.val)
+            else:
+                stack.append((node,True))
+                for c in node.children[::-1]:
+                    stack.append((c,False))
+        return res
