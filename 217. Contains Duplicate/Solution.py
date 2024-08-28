9d0b8fee2bc6eac6a12c5a03c63ddc1753be57c7
ak01a <96882081+ak01a@users.noreply.github.com>
2024-08-28 07:59:53 +0530

    Python3 Solution
    Submission Statistics:
    Question #: 217
    Language: Python3
    Runtime: 432 ms
    Runtime percentile: 21.89
    Memory: 31.96 MB
    Memory percentile: 48.47

--- 217. Contains Duplicate/Solution.py
+++ 217. Contains Duplicate/Solution.py
@@ -1,19 +1,20 @@
 class Solution:
     #TLE
     '''
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        n = len(nums)
-        for i in range(0,n):
-            for j in range(i+1,n):
-                if nums[i] == nums[j]:
-                    return True
-        return False
-
-    '''
-    def containsDuplicate(self, nums: List[int]) -> bool:
+   def containsDuplicate(self, nums: List[int]) -> bool:
         nums.sort()
         n = len(nums)
         for i in range(1,n):
             if nums[i] == nums[i-1]:
                 return True
         return False             
+    '''
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        s = set()
+        for i in nums:
+            if i in s:
+                return True
+            else:
+                s.add(i)
+
+        return False
