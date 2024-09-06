70a31a3fb259fd49691d5aef9fb2a7b1c9f3341e
ak01a <96882081+ak01a@users.noreply.github.com>
2024-09-06 09:41:45 +0530

    Python3 Solution
    Submission Statistics:
    Question #: 3501
    Language: Python3
    Runtime: 708 ms
    Runtime percentile: 80.33
    Memory: 54.94 MB
    Memory percentile: 70.63

---
+++ 3501. Delete Nodes From Linked List Present in Array/Solution.py
@@ -0,0 +1,20 @@
+#Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
+        nums = set(nums)
+        dummy = ListNode(0,head)
+
+        cur = head
+        prev = dummy
+        while cur:
+            if cur.val in nums:
+                prev.next = cur.next
+            else:
+                prev = prev.next
+            cur = cur.next
+        
+        return dummy.next
