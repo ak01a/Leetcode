b278a0ac19aab76e0a2954436c5bdb6fd0c2353f
Ananthakrishnan K <96882081+ak01a@users.noreply.github.com>
2024-09-22 01:49:48 +0530

    C++ Solution
    Submission Statistics:
    Question #: 2058
    Language: C++
    Runtime: 0 ms
    Runtime percentile: 100.00
    Memory: 15.596 MB
    Memory percentile: 27.96

--- 2058. Concatenation of Array/Solution.cpp
+++ 2058. Concatenation of Array/Solution.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            nums.push_back(nums[i]);
+        }
+        return nums;
+    }
+};
+    //OR
+    /*
         vector<int> ans;
         int n = nums.size();
         for(int i=0;i<n;i++){
@@ -11,4 +20,5 @@ public:
         }
         return ans;
     }
-};
+
+    */
