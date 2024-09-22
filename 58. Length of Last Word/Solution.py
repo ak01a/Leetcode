7e43bafd98f78df89e1937f09953baabc0451828
Ananthakrishnan K <96882081+ak01a@users.noreply.github.com>
2024-09-22 12:33:29 +0530

    Python3 Solution
    Submission Statistics:
    Question #: 58
    Language: Python3
    Runtime: 32 ms
    Runtime percentile: 74.70
    Memory: 16.6 MB
    Memory percentile: 58.75

--- 58. Length of Last Word/Solution.py
+++ 58. Length of Last Word/Solution.py
@@ -1,4 +1,17 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
+        # Split the string by whitespace and remove leading/trailing whitespace
+        words = s.strip().split()
+        
+        # If there are no words after splitting, return 0
+        if not words:
+            return 0
+        
+        # Return the length of the last word in the list of words
+        return len(words[-1])
+
+        #Mine :>
+        '''
         x = s.split()
-        return len(x[len(x)-1])
+        return len(x[len(x)-1]) 
+        '''
