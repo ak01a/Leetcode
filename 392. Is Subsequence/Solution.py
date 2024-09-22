bcf8f2366349e297e7f7dcbd54250953dd70aa19
Ananthakrishnan K <96882081+ak01a@users.noreply.github.com>
2024-09-22 12:11:57 +0530

    Python3 Solution
    Submission Statistics:
    Question #: 392
    Language: Python3
    Runtime: 27 ms
    Runtime percentile: 95.25
    Memory: 16.516 MB
    Memory percentile: 65.60

--- 392. Is Subsequence/Solution.py
+++ 392. Is Subsequence/Solution.py
@@ -2,18 +2,10 @@ class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         n = len(t)
         m = len(s)
-        j = 0
-        i = 0
+        i,j = 0,0
         while j<n and i<m:
             if s[i] == t[j]:
-                j+=1
                 i+=1
-            else:
-                j+=1
-
-        if i == m:
-            return True
-        else:
-            return False
-
+            j+=1
+        return i==m
 
