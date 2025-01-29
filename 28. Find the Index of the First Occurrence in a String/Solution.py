dab24890a295f34425c66c81df7ccb7a166a9cf0
Ananthakrishnan K <96882081+ak01a@users.noreply.github.com>
2025-01-29 21:16:43 +0530

    Python3 Solution
          Submission Statistics:
          Question #: 28
          Language: Python3
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 17.712 MB
          Memory percentile: 38.13

---
+++ 28. Find the Index of the First Occurrence in a String/Solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        for i in range(len(haystack) - len(needle) + 1):
+            for j in range(0,len(needle)):
+
+                if needle[j] != haystack[i+j]:
+                    break
+            else:
+                    return i
+        
+        return -1
