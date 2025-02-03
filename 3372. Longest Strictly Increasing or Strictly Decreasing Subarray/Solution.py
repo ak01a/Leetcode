f2b28c317ab4f5c1ec3ef0d9698968dd8fa3d9c4
Ananthakrishnan K <96882081+ak01a@users.noreply.github.com>
2025-02-03 20:43:03 +0530

    Python3 Solution
          Submission Statistics:
          Question #: 3372
          Language: Python3
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 17.904 MB
          Memory percentile: 11.65

---
+++ 3372. Longest Strictly Increasing or Strictly Decreasing Subarray/Solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestMonotonicSubarray(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        A = [1] * n #Longest strictly increasing sub array
+        for i in range(1,n):
+            if nums[i] > nums[i-1]:
+                A[i] += A[i-1]
+
+        B = [1] * n #Longest strictly decrasing sub array
+        for i in range(1,n):
+            if nums[i] < nums[i-1]:
+                B[i] += B[i-1]
+            
+        return max(max(A),max(B))
+        
